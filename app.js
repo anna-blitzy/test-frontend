9f3b02746b157ae0a2fad2b4b8c44e0d3e98cdff
ghonyana <artur@blitzy.com>
2025-10-24 11:35:17 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+document.getElementById('testBtn').addEventListener('click', () => {
+    alert('Frontend is working!');
+    console.log('Button clicked at:', new Date().toISOString());
+});
+
+console.log('Test Frontend loaded successfully');
